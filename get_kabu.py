55fac76727190721cb00b79fb962751e55efc060
DEETHIRATH <tonytonytony130@gmail.com>
2021-11-18 17:23:15 +0900

    2回目のtest

---
+++ get_kabu.py
@@ -0,0 +1,9 @@
+
+from selenium import webdriver
+from time import sleep
+
+driver = webdriver.Chrome('C:\data\chromedriver')
+url = "https://www.msn.com/ja-jp/money/stockdetails/fi-a9ihkr?ocid=ansMSNMoney11&duration=1D"
+
+driver.get(url)
+#
