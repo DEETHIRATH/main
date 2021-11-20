5cc11c3a9f8dcfea301031861165c68c0dd6aadd
DEETHIRATH <tonytonytony130@gmail.com>
2021-11-20 16:46:08 +0900

    企業名と株価をスクレイピングして表示

--- get_kabu.py
+++ get_kabu.py
@@ -1,9 +1,25 @@
 
-from selenium import webdriver
-from time import sleep
+import requests
+from bs4 import BeautifulSoup
+from tkinter import messagebox
 
-driver = webdriver.Chrome('C:\data\chromedriver')
-url = "https://www.msn.com/ja-jp/money/stockdetails/fi-a9ihkr?ocid=ansMSNMoney11&duration=1D"
+Address = "https://kabutan.jp/stock/?code="
+url_list = ["9318", "2038"]
+response = ""
 
-driver.get(url)
-#
+#会社名と株価を取得
+for url in url_list:
+    res = requests.get(Address + url)
+    soup = BeautifulSoup(res.text, "html.parser")
+    elems_corp_name = soup.select('#kobetsu')
+    elems_price = soup.select('#stockinfo_i1 > div.si_i1_2 > span.kabuka')
+    corp_name = elems_corp_name[0].contents[0].split()[0]
+    price = elems_price[0].contents[0]
+
+    result = corp_name + " : " + price + "\n"
+    response += result
+
+messagebox.showinfo('メッセージ', response)
+
+
+
